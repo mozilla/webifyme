c7c9c34dcc2b4a0a6dbf5893fc6dff4bd9e174e1
Adam Miller <adam@Adam-Millers-iMac.local>
2011-05-10 10:45:15 -0400

    removing some english strings from the JS

--- static/js/collage.js
+++ static/js/collage.js
@@ -469,16 +469,8 @@ things.BackgroundSelector = function(bgData) {
   }
   
   function init() {
-    buttonEl = $('<a class="change-background" href="#">Change the Background?</a>').appendTo('#header');
-    selectEl = $('\
-    <div class="background-selector">\
-			Pick a background\
-			<a class="up" href="#">Up</a>\
-			<div class="slide-wrapper">\
-				<ul></ul>\
-			</div>\
-			<a class="down" href="#">Down</a>\
-		</div>').insertAfter(buttonEl).hide();
+    buttonEl = $('#js-background-change-buttom').children().clone().appendTo('#header');
+    selectEl = $('#js-background-selector' ).children().clone().insertAfter(buttonEl).hide();
 		
 		var listEl = selectEl.find('.slide-wrapper ul');
 		for(var i = 0; i < backgroundData.length; i++) {
