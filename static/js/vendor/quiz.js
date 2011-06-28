d8e93d5b4dae038351e3f2260eb7684cf3f98e31
brez <jbresnik@barbariangroup.com>
2011-06-28 18:53:53 -0400

    adding additional validation to title to cover spaces

--- static/js/vendor/quiz.js
+++ static/js/vendor/quiz.js
@@ -323,7 +323,11 @@ things.QuizForm = function(el) {
   
   function validate() {
     var name_re = /^[A-Za-z0-9_\'\-!\. ]{1,15}$/
+    var spaces_re = /^\s+$/
     if( ! name_re.test(data['username'])) {
+      addError('name', $._( 'msg-name-required-error' ) );
+    }
+    if( spaces_re.test(data['username'])) {
       addError('name', $._( 'msg-name-required-error' ) );
     }
     
